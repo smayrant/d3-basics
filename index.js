fec79b6bdbfa8a499910e2a4e81fbac0bb65474e
Sheldrick <mrmayrant@yahoo.com>
2019-04-22 03:29:05 -0400

    grabbed data from a JSON file to create SVG circles

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const canvas = d3.select('.canvas');
-const svg = canvas.append('svg')
-    .attr('height', 600)
-    .attr('width', 600);
+// select svg container
+const svg = d3.select('svg');
 
-const group = svg.append('g')
-    .attr('transform', 'translate(20, 100)')
+d3.json('planets.json').then(data => {
+    const circs = svg.selectAll('circle')
+        .data(data);
 
-// append shapes to svg container
-group.append('rect')
-    .attr('width', 200)
-    .attr('height', 100)
-    .attr('fill', 'blue')
-    .attr('x', 20)
-    .attr('y', 20);
+    // add attrs to circs already in the DOM
+    circs.attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d => d.radius)
+        .attr('fill', d => d.fill);
 
-group.append('circle')
-    .attr('r', 50)
-    .attr('cx', 300)
-    .attr('cy', 70)
-    .attr('fill', 'pink');
-
-group.append('line')
-    .attr('x1', 380)
-    .attr('x2', 380)
-    .attr('y1', 20)
-    .attr('y2', 125)
-    .attr('stroke', 'red')
-    .attr('stroke-width', 5);
-
-group.append('text')
-    .attr('x', 20)
-    .attr('y', 200)
-    .attr('fill', 'grey')
-    .text('The basics of D3')
-    .style('font-size', '45')
+    // append the enter selection to the DOM
+    circs.enter()
+        .append('circle')
+        .attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d => d.radius)
+        .attr('fill', d => d.fill);
+})
