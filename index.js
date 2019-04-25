5e0871568d5901d9d9f5e2c552679f074bef3217
Sheldrick <mrmayrant@yahoo.com>
2019-04-25 15:57:31 -0400

    created a band scale, updated linear scale using min and max and grouped the bar chart and added margins

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // select svg container
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+// create margins and dimensions
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+// the container's width minus the left and right margins creates the graph's width
+const graphWidth = 600 - margin.left - margin.right;
+// the container's height minus the top and bottom margins creates the graph's height
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`)
 
 d3.json('menu.json').then(data => {
 
@@ -8,23 +23,33 @@ d3.json('menu.json').then(data => {
         .domain([0, 1000])
         .range([0, 500]);
 
+    const min = d3.min(data, d => d.orders)
+    const max = d3.max(data, d => d.orders)
+
+    // create a band scale
+    const x = d3.scaleBand()
+        .domain(data.map(item => item.name))
+        .range([0, 500])
+        .paddingInner(0.2)
+        .paddingOuter(0.2)
+
     //  join the data to the rects
-    const rects = svg.selectAll('rect')
+    const rects = graph.selectAll('rect')
         .data(data)
 
     // update rects currently in the DOM
-    rects.attr('width', 50)
+    rects.attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
         // ensures the rects have space in between
-        .attr('x', (d, i) => i * 70)
+        .attr('x', d => x(d.name))
 
     // append the enter selection to the DOM
     rects.enter()
         .append('rect')
-        .attr('width', 50)
+        .attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
         // ensures the rects have space in between
-        .attr('x', (d, i) => i * 70)
+        .attr('x', d => x(d.name))
 })
