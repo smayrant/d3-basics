030d1c601f19946a2908b900dbf3fe558e70597b
Sheldrick <mrmayrant@yahoo.com>
2019-04-26 16:42:59 -0400

    retrieved data from Firestore database instead of JSON data

--- index.js
+++ index.js
@@ -16,12 +16,21 @@ const graph = svg.append('g')
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left},${margin.top})`)
 
-d3.json('menu.json').then(data => {
+const xAxisGroup = graph.append('g')
+    // the x axis group is moved to the bottom of the graph
+    .attr('transform', `translate(0, ${graphHeight})`)
+const yAxisGroup = graph.append('g');
 
-    // create a linear scale for the y-axis
+// retrieve data from Firestore DB
+db.collection('dishes').get().then(res => {
+    let data = [];
+    res.docs.forEach(doc => {
+        data.push(doc.data())
+    })
+    // create a linear scale for the y - axis
     const y = d3.scaleLinear()
-        .domain([0, 1000])
-        .range([0, 500]);
+        .domain([0, d3.max(data, d => d.orders)])
+        .range([graphHeight, 0]);
 
     const min = d3.min(data, d => d.orders)
     const max = d3.max(data, d => d.orders)
@@ -39,17 +48,34 @@ d3.json('menu.json').then(data => {
 
     // update rects currently in the DOM
     rects.attr('width', x.bandwidth)
-        .attr('height', d => y(d.orders))
+        .attr('height', d => graphHeight - y(d.orders))
         .attr('fill', 'orange')
         // ensures the rects have space in between
         .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
 
     // append the enter selection to the DOM
     rects.enter()
         .append('rect')
         .attr('width', x.bandwidth)
-        .attr('height', d => y(d.orders))
+        .attr('height', d => graphHeight - y(d.orders))
         .attr('fill', 'orange')
         // ensures the rects have space in between
         .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
+
+    // create the axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y)
+        .ticks(5)
+        .tickFormat(d => d + ' orders');
+
+    // call the axes
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    // select text from the x axis group and rotate it -40deg using the text anchor as the end instead of the default middle
+    xAxisGroup.selectAll('text')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
 })
